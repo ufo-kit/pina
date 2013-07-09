c352ca5595c034b6bc9d175b0597673377369a24
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-07-09 21:58:38 +0200

    Instantiate qualifier object if class is used

--- pyfo/misc.py
+++ pyfo/misc.py
@@ -5,11 +5,23 @@ from .qualifiers import AddressSpaceQualifier, NoQualifier
 
 def source(*args):
     def _source(func):
-        qual_args = [arg if isinstance(arg, AddressSpaceQualifier)
-                     else NoQualifier(arg)
-                     for arg in args]
+        qual_args = []
+
+        for arg in args:
+            if isinstance(arg, AddressSpaceQualifier):
+                qual_args.append(arg)
+            elif isinstance(arg, AddressSpaceQualifier.__class__):
+                # Someone passed in the class name without constructing a new
+                # qualifier object (e.g. @source(Constant)), in this case we
+                # assume float type and instantiate a new qualifier.
+                qual_args.append(arg(float))
+            else:
+                qual_args.append(NoQualifier(arg))
+
         return make_kernel(func, qual_args)
 
+    # The decorator was instantiated without any arguments. In this case
+    # args[0] is the decorated function!
     if len(args) == 1 and isinstance(args[0], types.FunctionType):
         return make_kernel(args[0])
 
