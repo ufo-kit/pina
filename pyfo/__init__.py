f89095d04b48cdc9fd8871c51fbcdd5aa2d4a54c
Matthias Vogelgesang <matthias.vogelgesang@kit.edu>
2014-01-29 09:15:26 +0100

    Export ExecutionEnvironment in top-level package

--- pyfo/__init__.py
+++ pyfo/__init__.py
@@ -4,3 +4,4 @@ from .gen import kernel
 from .misc import static, jit
 from .qualifiers import Global, Constant, Local
 from .qualifiers import set_default_float_type
+from .cl import ExecutionEnvironment
